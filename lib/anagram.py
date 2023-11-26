9c732e94cc0fa05cc83a1230683a3049e533d4a1
Charlie <cnewlinds@gmail.com>
2023-11-26 21:06:27 +1100

    test pass

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagram):
+        return [anagram for anagram in possible_anagram if self.is_anagram(anagram)]
+    
+    def is_anagram(self, candidate):
+        candidate = candidate.lower()
+        return sorted(candidate) == sorted(self.word) and candidate != self.word
